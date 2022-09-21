76bab2861d511e342ed9f1de76d26384cac0fb1d
Jake Overall <joverall22@gmail.com>
2022-09-21 14:47:21 -0600

    neato

--- server/controllers/AccountController.js
+++ server/controllers/AccountController.js
@@ -1,5 +1,6 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
+import { coursesService } from '../services/CoursesService.js'
 import BaseController from '../utils/BaseController'
 
 export class AccountController extends BaseController {
@@ -8,6 +9,8 @@ export class AccountController extends BaseController {
     this.router
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
+      .get('/courses', this.getMyCourses)
+      .post('/enroll', this.enrollInCourse)
   }
 
   async getUserAccount(req, res, next) {
@@ -18,4 +21,29 @@ export class AccountController extends BaseController {
       next(error)
     }
   }
+
+  async getMyCourses(req, res, next) {
+    try {
+      const courses = await coursesService.getCoursesByStudentId(req.userInfo.id)
+      res.send(courses)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+
+  async enrollInCourse(req, res, next) {
+    try {
+      //       V NOTE remember this must match the schema
+      req.body.studentId = req.userInfo.id
+      const studentEnrollment = await coursesService.enrollInCourse(req.body)
+      res.send(studentEnrollment)
+    } catch (error) {
+      next(error)
+    }
+
+  }
+
+
+
 }
--- server/services/CoursesService.js
+++ server/services/CoursesService.js
@@ -1,6 +1,19 @@
 import { dbContext } from "../db/DbContext.js"
 
 class CoursesService {
+  async getCoursesByStudentId(studentId) {
+    const courses = await dbContext.StudentEnrollments.find({ studentId }).populate('course', 'name code').populate('student', 'name picture')
+    return courses
+  }
+
+  async enrollInCourse(formData) {
+    const enrollment = await dbContext.StudentEnrollments.create(formData)
+
+    await enrollment.populate('course', 'name code')
+
+    return enrollment
+
+  }
 
   async get() {
     const courses = await dbContext.Courses.find()
